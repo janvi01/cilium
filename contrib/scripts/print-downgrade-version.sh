janvi01/cilium

commit 5581963cbf9489d980b9b5a8ccf1c5e017e35d3c

ci/ipsec: Fix version retrieval for downgrades to closest patch release
This commit brings two fixes to the script that we use to determine to
which version we should upgrade/downgrade in some CI workflows.

The first fix is the most important one. When looking for the closest
patch version, make the script return the value in VERSION instead of
decrementing it. The rationale is that for stable branches, VERSION
already points to the latest patch release, there is no need to decrease
it further! This fix does not affect the output for the calculation of
the previous minor version number.

The second fix is simply the addition of an error message in case the
minor version number is 0, to get some explicit error instead of a
silent failure if we ever reach Cilium 2.0.0.

Updated samples of numbers from VERSION and the corresponding values
returned:

    VERSION         Previous minor  Previous patch release

    1.14.3          v1.13           v1.14.3
    1.14.1          v1.13           v1.14.1
    1.14.0          v1.13           <error>
    1.14.1-dev      v1.13           v1.14.1
    1.15.0-dev      v1.14           <error>
    1.13.90         v1.12           <error>
    2.0.1           <error>         v2.0.1

Fixes: 56dfec2f1ac5 ("contrib/scripts: Support patch releases in print-downgrade-version.sh")
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

contrib/scripts/print-downgrade-version.sh
@@ -25,11 +25,14 @@ if [[ ${1-} == "patch" ]] ; then
             exit 1
             ;;
         *)
-            ((patch--))
             echo "v${major}.${minor}.${patch}${TAG_SUFFIX:-}"
             ;;
     esac
 else
+    if [[ "${minor}" == "0" ]] ; then
+        >&2 echo "ERROR: failed to deduce release previous to version '$VERSION'"
+        exit 1
+    fi
     # Else print the previous stable version by decrementing the minor version
     # and trimming the patch version.
     ((minor--))

commit fe446ba177c1e95ccfe91913a3678e3b6e3b7962

ci/ipsec: Fix downgrade version for release preparation commits
For the IPsec upgrade/downgrade CI test, for the jobs where we
upgrade/downgrade to the closest patch release, the script
print-downgrade-version.sh determines the patch release by picking up
the value in file VERSION. This works most of the time.

For release preparation commits, however, this approach fails because
the reference in VERSION points to a release that has not been tagged or
published yet. We need to pick the previous patch release in that case.

However, it's non-trivial to figure out whether we're on a release
preparation commit, in particular because the CI workflow does a shallow
Git clone and we don't have access to the Git history. I couldn't find
an ideal solution, so this commit changes the shallow-clone into a clone
with 2 commits (one for the last commit, which is the one we're
interested in, and another one squashing all the rest of the history).
And we add a hack in the script to account for this last commit and
check whether it modifies the VERSION file. If it does, we decrement the
version found in VERSION before computing the patch release to downgrade
to.

Fixes: 5581963cbf94 ("ci/ipsec: Fix version retrieval for downgrades to closest patch release")
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

.github/workflows/tests-ipsec-upgrade.yaml
@@ -153,6 +153,11 @@ jobs:
         uses: actions/checkout@b4ffde65f46336ab88eb53be808477a3936bae11 # v4.1.1
         with:
           ref: ${{ inputs.context-ref || github.sha }}
+          # We need some context on the last commit to determine whether we're
+          # on a release preparation commit, in which case the script
+          # print-downgrade-version.sh needs to adjust the patch release number
+          # to downgrade to.
+          fetch-depth: 2
           persist-credentials: false
 
       - name: Set Environment Variables

contrib/scripts/print-downgrade-version.sh
@@ -19,15 +19,24 @@ fi
 
 if [[ ${1-} == "patch" ]] ; then
     # If user passed "patch" as first argument, print the latest patch version
-    case ${patch} in
-        0)
-            >&2 echo "ERROR: failed to deduce patch release previous to version '$VERSION'"
-            exit 1
-            ;;
-        *)
-            echo "v${major}.${minor}.${patch}${TAG_SUFFIX:-}"
-            ;;
-    esac
+
+    # Hack: When working on a patch release preparation commit, file VERSION
+    # contains the new value for the release that is yet to be tagged and
+    # published. In this case, we want to downgrade to the previous patch
+    # release.
+    # Skip this step if we're not in a Git repository or if there is no more
+    # than one commit (likely a shallow clone).
+    if git rev-parse --is-inside-work-tree &> /dev/null && \
+        git rev-parse --verify HEAD^ &> /dev/null && \
+        git diff --name-only HEAD^..HEAD | grep -q "^VERSION$"; then
+        patch=$((patch - 1))
+    fi
+    if [[ "${patch}" -le "0" ]] ; then
+        >&2 echo "ERROR: failed to deduce patch release previous to version '$VERSION'"
+        exit 1
+    else
+        echo "v${major}.${minor}.${patch}${TAG_SUFFIX:-}"
+    fi
 else
     if [[ "${minor}" == "0" ]] ; then
         >&2 echo "ERROR: failed to deduce release previous to version '$VERSION'"